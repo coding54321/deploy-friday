163d01e75db2cab8bf881898233104abe6984aaa
01tilinfinity <kuwinter0209@gmail.com>
2024-07-24 03:38:10 +0900

    7

--- cloudtype-production/accounts/forms.py
+++ cloudtype-production/accounts/forms.py
@@ -6,10 +6,11 @@ class CafeCreationForm(forms.ModelForm):
     """A form for creating new cafes. Includes all the required fields, plus a repeated password."""
     password1 = forms.CharField(label='Password', widget=forms.PasswordInput)
     password2 = forms.CharField(label='Password confirmation', widget=forms.PasswordInput)
+    cafe_photo = forms.ImageField(required=False)
 
     class Meta:
         model = Cafe
-        fields = ('cafe_id', 'cafe_name', 'telephone')
+        fields = ('cafe_id', 'cafe_name', 'telephone', 'cafe_photo')
 
     def clean_password2(self):
         # Check that the two password entries match
@@ -30,17 +31,13 @@ class CafeCreationForm(forms.ModelForm):
 class CafeChangeForm(forms.ModelForm):
     """A form for updating cafes. Includes all the fields on the cafe, but replaces the password field with admin's password hash display field."""
     password = ReadOnlyPasswordHashField()
+    cafe_photo = forms.ImageField(required=False)
 
     class Meta:
         model = Cafe
-        fields = ('cafe_id', 'cafe_name', 'telephone', 'password', 'is_superuser')
+        fields = ('cafe_id', 'cafe_name', 'telephone', 'password', 'is_superuser', 'cafe_photo')
 
     def clean_password(self):
         # Regardless of what the user provides, return the initial value.
         # This is done here, rather than on the field, because the field does not have access to the initial value
         return self.initial["password"]
-
-
-class CafeLoginForm(forms.Form):
-    cafe_id = forms.CharField(max_length=100, label='Cafe ID')
-    password = forms.CharField(widget=forms.PasswordInput, label='Password')
